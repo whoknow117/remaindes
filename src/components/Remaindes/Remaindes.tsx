78467269c910e4dfd8acc6211424b21b62e260e0
newbeeguy <mskjavascript@gmail.com>
2021-02-20 07:53:31 +0300

    change button desighn

--- src/components/Remaindes/Remaindes.tsx
+++ src/components/Remaindes/Remaindes.tsx
@@ -56,7 +56,7 @@ const   Remaindes: React.FC<RemaindesType> = ( ) => {
 
 
                            <div className={classes.valueWrapper}>
-                               <button onClick={() => {
+                               <button className={classes.delete} onClick={() => {
                                dispatch(DeleteValueAC(h.id))}
                                }>x</button>
                                <div className={classes.title}>{h.title} </div>
